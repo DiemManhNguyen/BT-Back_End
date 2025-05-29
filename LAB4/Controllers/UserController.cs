51ec45f184bee06713929c536348260c43ab9d4a
DiemManhNguyen <diemmanhnguyen115@gmail.com>
2025-05-30 00:24:41 +0700

    first commit

---
+++ LAB4/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿// Controllers/UserController.cs
+using System.Web.Mvc;
+
+public class UserController : Controller
+{
+    private static List<User> _users = new List<User>(); // Lưu trữ tạm trong bộ nhớ
+
+    // GET: User/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: User/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            // Thêm người dùng vào danh sách tạm
+            _users.Add(user);
+
+            // Hiển thị thông báo thành công
+            TempData["Message"] = "Thêm người dùng thành công!";
+            return RedirectToAction("List"); // Chuyển đến trang danh sách
+        }
+
+        return View(user);
+    }
+
+    // GET: User/List
+    public ActionResult List()
+    {
+        return View(_users);
+    }
+}
---
+++ LAB4/Models/User.cs
@@ -0,0 +1,26 @@
+﻿// Models/User.cs
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    [Required(ErrorMessage = "Không được để trống!")]
+    [Display(Name = "Mã sinh viên")]
+    public string StudentId { get; set; }
+
+    [Required(ErrorMessage = "Không được để trống!")]
+    [StringLength(20, MinimumLength = 6, ErrorMessage = "Tài khoản từ 6 - 20 ký tự")]
+    [Display(Name = "Tài khoản")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Không được để trống!")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Mật khẩu")]
+    public string Password { get; set; }
+
+    [Display(Name = "Điện thoại")]
+    public string Phone { get; set; }
+
+    [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+}
